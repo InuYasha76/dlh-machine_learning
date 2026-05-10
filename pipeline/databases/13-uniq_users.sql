0f18857e5a61721017e147d82270d698ee39e64f
gcanavag <gcanavag@student.42luxembourg.lu>
2026-05-11 00:54:08 +0200

    Tried other syntax

--- pipeline/databases/13-uniq_users.sql
+++ pipeline/databases/13-uniq_users.sql
@@ -1,13 +1,7 @@
--- creates a table users (
---		id, int, not null, auto-increment, pki
---		email, string 255, never null, unique
--- 		name string 255)
+-- Create users table
 -- script should not fail if table already exists
--- make attribute unique directly in the table schema
 CREATE TABLE IF NOT EXISTS users (
-	id INT NOT NULL AUTO_INCREMENT,
-	email VARCHAR(255) NOT NULL,
-	name VARCHAR(255),
-	PRIMARY KEY (id),
-	UNIQUE (email)
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
 );
