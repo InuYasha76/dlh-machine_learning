8f8562ad2b4d7534d1cd0681369e75b481ed07b8
gcanavag <gcanavag@student.42luxembourg.lu>
2026-05-10 23:23:22 +0200

    Fix typ[o

--- pipeline/databases/6-avg_temperatures.sql
+++ pipeline/databases/6-avg_temperatures.sql
@@ -1,3 +1,6 @@
 -- Displays the average temperature (Fahrenheit) by city
 -- Orders display by temperature (descending).
-SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp_in_F DESC;
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC;
