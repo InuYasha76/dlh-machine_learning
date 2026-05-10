0dfb68512dca8c373bda60a0968ddd7424ab1fb7
gcanavag <gcanavag@student.42luxembourg.lu>
2026-05-11 00:30:19 +0200

    Try LEFT JOIN

--- pipeline/databases/12-rating_genres.sql
+++ pipeline/databases/12-rating_genres.sql
@@ -5,9 +5,9 @@ SELECT
     tg.name AS name, 
     COALESCE(SUM(tsr.rate), 0) AS rating
 FROM tv_genres tg
-OUTER JOIN tv_show_genres tsg 
+LEFT JOIN tv_show_genres tsg 
     ON tg.id = tsg.genre_id
-OUTER JOIN tv_show_ratings tsr
+LEFT JOIN tv_show_ratings tsr
 	ON tsg.show_id = tsr.show_id
 GROUP BY name
 ORDER BY rating DESC;
