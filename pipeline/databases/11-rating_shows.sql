f8e29d21796339139d4afb30f5b866f5f825ee49
gcanavag <gcanavag@student.42luxembourg.lu>
2026-05-11 01:08:47 +0200

    Trying to get 100%

--- pipeline/databases/11-rating_shows.sql
+++ pipeline/databases/11-rating_shows.sql
@@ -1,11 +1,8 @@
 -- lists all shows from hbtn_0d_tvshows_rate by their rating
--- Results must be displayed as title, rating
--- Results must be sorted in descending order by the rating
-SELECT 
-    ts.title AS title, 
-    SUM(tsr.rate) AS rating
+-- Results are displayed as title, rating
+-- Results are sorted in descending order by the rating
+SELECT ts.title AS title, SUM(tsr.rate) AS rating
 FROM tv_shows ts
-INNER JOIN tv_show_ratings tsr 
-    ON ts.id = tsr.show_id
+INNER JOIN tv_show_ratings tsr ON ts.id = tsr.show_id
 GROUP BY ts.title
 ORDER BY rating DESC;
