332c611b2fe64f9ba60864076c1554d0be09cf67
gcanavag <gcanavag@student.42luxembourg.lu>
2026-05-10 23:18:56 +0200

    Fix task 5, rem ROUND()

--- pipeline/databases/5-average.sql
+++ pipeline/databases/5-average.sql
@@ -1,3 +1,3 @@
 -- computes the score average of all records in the table second_table
 -- The result column name is average
-SELECT ROUND(AVG(score), 2) AS average FROM second_table;
+SELECT AVG(score) AS average FROM second_table;
