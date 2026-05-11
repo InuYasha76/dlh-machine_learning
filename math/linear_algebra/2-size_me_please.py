aebb5943cc75f682d0791fc7ea990ae9bd2b0e1a
gcanavag <gcanavag@student.42luxembourg.lu>
2026-05-11 21:15:18 +0200

    Add task 2

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""This module id about  matrices"""
+
+
+def matrix_shape(matrix):
+    """Calculates the shape of a matrix"""
+    try:
+        shape = [len(matrix), len(matrix[0]), len(matrix[0][0])]
+    except Exception:
+        try:
+            shape = [len(matrix), len(matrix[0])]
+        except Exception:
+            raise
+    finally:
+        return shape:
