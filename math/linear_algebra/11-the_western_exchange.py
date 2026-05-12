be516b4bfe3b149df851484b9d9e769696d2f633
gcanavag <gcanavag@student.42luxembourg.lu>
2026-05-12 22:07:02 +0200

    Add task#11

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""This module is about numpy ndarrays."""
+
+
+import numpy as np
+
+
+def np_transpose(matrix):
+    """Returns a new numpy array that is the transpose of matrix."""
+    return matrix.T.copy()
