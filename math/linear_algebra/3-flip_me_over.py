e54a0e35eae201fef17fed1a215c3c2a08d864ab
gcanavag <gcanavag@student.42luxembourg.lu>
2026-05-12 02:05:09 +0200

    Add 3&4

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""This module is about matrices"""
+
+
+def matrix_transpose(matrix):
+    """Returns a transposed matrix"""
+    if not(matrix and 
+           isinstance(matrix, list) and 
+           isinstance(matrix[0], list)):
+        return matrix
+    new_list = [matrix_transpose(sub_list) for sub_list in matrix]
+    return [list(tuples) for tuples in zip(*new_list)]
+
+if __name__ == "__main__":
+    mat1 = [[1, 2],
+            [3, 4]]
+    print(mat1)
+    print(matrix_transpose(mat1))
+    print()
+    mat2 = [[1, 2, 3, 4, 5],
+            [6, 7, 8, 9, 10],
+            [11, 12, 13, 14, 15],
+            [16, 17, 18, 19, 20],
+            [21, 22, 23, 24, 25],
+            [26, 27, 28, 29, 30]]
+    print(mat2)
+    print(matrix_transpose(mat2))
+    print()
+    mat3 = [
+            [[1, 2, 3, 4, 5],
+             [6, 7, 8, 9, 10],
+             [11, 12, 13, 14, 15]],
+            [[16, 17, 18, 19, 20],
+             [21, 22, 23, 24, 25],
+             [26, 27, 28, 29, 30]]
+            ]
+    print(mat3)
+    print(matrix_transpose(mat3))
---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""This module is about Linar Algebra."""
+
+
+def add_arrays(arr1, arr2):
+    """Adds two arrays element-wise"""
+    if len(arr1) != len(arr2):
+        return None
+    return [sum(items) for items in zip(arr1, arr2)]
+
+if __name__ == "__main__":
+    arr1 = [1, 2, 3, 4]
+    arr2 = [5, 6, 7, 8]
+    print(add_arrays(arr1, arr2))
+    print(arr1)
+    print(arr2)
+    print(add_arrays(arr1, [1, 2, 3]))
