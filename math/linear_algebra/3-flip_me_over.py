ee4894bb9ec73f53395a3c58d7ffd6a412fee800
gcanavag <gcanavag@student.42luxembourg.lu>
2026-05-12 08:24:21 +0200

    Rem too long lines

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -4,23 +4,21 @@
 
 def matrix_transpose(matrix):
     """Returns a transposed matrix"""
-    if not(matrix and 
-           isinstance(matrix, list) and 
-           isinstance(matrix[0], list)):
-        return matrix
     return list(zip(*matrix))
 
+
 if __name__ == "__main__":
-    mat1 = [[1, 2],
-            [3, 4]]
+    mat1 = [[1, 2], [3, 4]]
     print(mat1)
     print(matrix_transpose(mat1))
     print()
-    mat2 = [[1, 2, 3, 4, 5],
-            [6, 7, 8, 9, 10],
-            [11, 12, 13, 14, 15],
-            [16, 17, 18, 19, 20],
-            [21, 22, 23, 24, 25],
-            [26, 27, 28, 29, 30]]
+    mat2 = [
+        [1, 2, 3, 4, 5],
+        [6, 7, 8, 9, 10],
+        [11, 12, 13, 14, 15],
+        [16, 17, 18, 19, 20],
+        [21, 22, 23, 24, 25],
+        [26, 27, 28, 29, 30],
+    ]
     print(mat2)
     print(matrix_transpose(mat2))
