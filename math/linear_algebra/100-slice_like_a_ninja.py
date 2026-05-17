aecd0684e95a7e6dd6e565909e05fd59a55db138
gcanavag <gcanavag@student.42luxembourg.lu>
2026-05-17 20:12:06 +0200

    Add task 100

---
+++ math/linear_algebra/100-slice_like_a_ninja.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""This module is about ndarrays slicing."""
+
+import numpy as np
+
+
+def np_slice(matrix, axes={}):
+    """
+    Slices a matrix along specific axes.
+    Args:
+        matrix: numpy array,
+        axes: dictionary.
+            key: defines the axis,
+            value: defines start, end, step.
+    Returns:
+        A numpy array.
+    """
+    # Init a list of full slices (:) for every dimension of the matrix
+    slice_list = [slice(None)] * matrix.ndim
+    # Override the specific axes provided in the dictionary
+    for axis, slice_args in axes.items():
+        if axis < matrix.ndim:
+            slice_list[axis] = slice(*slice_args)
+    return matrix[tuple(slice_list)]
