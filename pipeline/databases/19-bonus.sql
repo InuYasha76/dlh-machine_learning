a5ab0bc7cde0b7ceb237a6925571a9f3ed313cf0
Inuyashas-git <guillaume@Guillaumes-MacBook-Pro.local>
2026-05-21 14:29:58 +0200

    Add task 19

---
+++ pipeline/databases/19-bonus.sql
@@ -0,0 +1,21 @@
+-- Creates stored procedure AddBonus that adds a new correction for a student.
+
+DELIMITER //
+
+CREATE PROCEDURE AddBonus(
+	IN user_id INT,
+	IN project_name VARCHAR(255),
+	IN score INT
+)
+BEGIN
+	DECLARE project_id INT;
+
+	SELECT projects.id INTO project_id
+	FROM projects
+	WHERE projects.name = project_name;
+
+	INSERT INTO corrections (user_id, project_id, score)
+	VALUES (user_id, project_id, score);
+END //
+
+DELIMITER ;
