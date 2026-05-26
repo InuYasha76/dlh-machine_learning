099f7591762eafe9cacf5700168e42f88e1afaeb
gcanavag <gcanavag@student.42luxembourg.lu>
2026-05-26 16:21:48 +0200

    Add task 0

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -1,13 +1,42 @@
 #!/usr/bin/env python3
+"""This module is about matrices an determinant."""
+
+
+def get_minor(matrix, i, j):
+    """Extracts the submatrix by removing the i-th row and the j-th column."""
+    remaining_rows = matrix[:i] + matrix[i + 1:]
+    return [row[:j] + row[j + 1:] for row in remaining_rows]
 
 
 def determinant(matrix):
-    if not isinstance(matrix[0], list):
+    """
+    Calculates the determinant of a matrix recursively.
+    Args:
+        - matrix (list of lists).
+    Returns:
+        - the determinant of matrix.
+    """
+    n = len(matrix)
+    det = 0
+    sign = 1
+
+    if not (matrix and
+            isinstance(matrix, list) and
+            isinstance(matrix[0], list)):
         raise TypeError("matrix must be a list of lists")
-    if len(matrix) != len(matrix[0]):
+
+    if n != len(matrix[0]):
         raise ValueError("matrix must be a square matrix")
-    if len(matrix) == 1 and len(matrix[0]) == 1:
-        return 0 if matrix[0][0] is None else matrix[0][0]
-    
 
+    if n == 0 or len(matrix[0]) == 0:
+        return 1
 
+    if n == 1:
+        return matrix[0][0]
+
+    for i in range(n):
+        sub_matrix = get_minor(matrix, i)
+        det += sign * matrix[0][i] * determinant(sub_matrix)
+        sign *= -1
+
+    return det
