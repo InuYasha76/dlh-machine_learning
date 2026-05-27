6e5ffbe2fa755a87b69a09ee1132055f64d0fef0
gcanavag <gcanavag@student.42luxembourg.lu>
2026-05-27 17:26:46 +0200

    Changed the param name to avoid confusion with user_id attribute

--- pipeline/databases/103-average_weighted_score.sql
+++ pipeline/databases/103-average_weighted_score.sql
@@ -1,8 +1,7 @@
 -- Creates a stored procedure that computes and stores the average weighted score for a student.
-
 DELIMITER //
 
-CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN input_user_id INT)
 BEGIN
     UPDATE users u
     LEFT JOIN (
@@ -11,11 +10,11 @@ BEGIN
             SUM(cor.score * p.weight) / SUM(p.weight) AS weighted_avg_score
         FROM corrections cor
         INNER JOIN projects p ON cor.project_id = p.id
-        WHERE cor.user_id = ComputeAverageWeightedScoreForUser.user_id
+        WHERE cor.user_id = input_user_id
         GROUP BY cor.user_id
     ) c ON c.user_id = u.id
     SET u.average_score = COALESCE(c.weighted_avg_score, 0)
-    WHERE u.id = ComputeAverageWeightedScoreForUser.user_id;
+    WHERE u.id = input_user_id;
 END //
 
 DELIMITER ;
