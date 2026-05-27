3701ca45fdecae3bb2d57c5a0046428272b9309b
gcanavag <gcanavag@student.42luxembourg.lu>
2026-05-27 17:22:31 +0200

    Typo in the name of the procedure

--- pipeline/databases/103-average_weighted_score.sql
+++ pipeline/databases/103-average_weighted_score.sql
@@ -1,21 +1,21 @@
--- Creates a stored procedure that computes and store the average weighted score for a student.
+-- Creates a stored procedure that computes and stores the average weighted score for a student.
 
 DELIMITER //
 
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
 BEGIN
     UPDATE users u
     LEFT JOIN (
-        SELECT
-			cor.user_id,
-			SUM(cor.score * p.weight) / SUM(p.weight) AS weighted_avg_score
+        SELECT 
+            cor.user_id,
+            SUM(cor.score * p.weight) / SUM(p.weight) AS weighted_avg_score
         FROM corrections cor
-		INNER JOIN projects p ON cor.project_id = p.id
-        WHERE cor.user_id = ComputeAverageScoreForUser.user_id
+        INNER JOIN projects p ON cor.project_id = p.id
+        WHERE cor.user_id = ComputeAverageWeightedScoreForUser.user_id
         GROUP BY cor.user_id
     ) c ON c.user_id = u.id
     SET u.average_score = COALESCE(c.weighted_avg_score, 0)
-	WHERE u.user_id = ComputeAverageScoreForUser.user_id;
+    WHERE u.id = ComputeAverageWeightedScoreForUser.user_id;
 END //
 
 DELIMITER ;
