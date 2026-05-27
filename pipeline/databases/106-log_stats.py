dbf93b2678e4c444cd61c374a0b91a491ab08761
gcanavag <gcanavag@student.42luxembourg.lu>
2026-05-27 15:13:46 +0200

    Add 106

---
+++ pipeline/databases/106-log_stats.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+Module that provides statistics about Nginx logs stored in MongoDB.
+"""
+from pymongo import MongoClient
+
+
+def log_stats():
+    """
+    Provides some stats about Nginx logs stored in MongoDB.
+    """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+    total_logs = nginx_collection.count_documents({})
+    print(f"{total_logs} logs")
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = nginx_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+    status_check = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
+    print(f"{status_check} status check")
+    print("IPs:")
+    pipeline = [
+            {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+            {"$sort": {"count": -1}},
+            {"$limit": 10}
+            ]
+    ips = nginx_collection.aggregate(pipeline)
+    for ip in ips:
+        print(f"\t{ip['_id']}: {ip['count']}")
+
+
+if __name__ == "__main__":
+    log_stats()
