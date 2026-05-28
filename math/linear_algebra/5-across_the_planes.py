340680637e779d6dcb7060bbdd9d78d46be0b73d
gcanavag <gcanavag@student.42luxembourg.lu>
2026-05-28 11:18:55 +0200

    Fix 5

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -2,15 +2,12 @@
 """This module is about linear algebra."""
 
 
-def matrix_shape(matrix):
-    """Calculates the shape of a matrix recursively"""
-    if not (matrix and isinstance(matrix, list)):
-        return []
-    return [len(matrix)] + matrix_shape(matrix[0])
-
-
 def add_matrices2D(mat1, mat2):
     """Adds two matrices element-wise."""
-    if matrix_shape(mat1) != matrix_shape(mat2):
+    try:
+        return [
+                [a + b for a, b in zip(r1, r2, strict=True)]
+                for r1, r2 in zip(mat1, mat2, strict=True)
+                ]
+    except ValueError:
         return None
-    return [[a + b for a, b in zip(r1, r2)] for r1, r2 in zip(mat1, mat2)]
