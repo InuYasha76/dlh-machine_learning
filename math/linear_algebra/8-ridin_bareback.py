9c85afcbb4aec61a1edc1a6537d0e5e8c7616cd8
gcanavag <gcanavag@student.42luxembourg.lu>
2026-05-28 12:05:12 +0200

    Fix 8

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -3,10 +3,18 @@
 
 
 def mat_mul(mat1, mat2):
-    """Performs matrix multiplication:"""
+    """Performs matrix multiplication."""
+
+    if not mat1 or not mat2 or not mat1[0] or not mat2[0]:
+        return None
+
     if len(mat1[0]) != len(mat2):
         return None
+
     mat2_t = list(zip(*mat2))
-    return [[sum(a * b for a, b in zip(row, col))
-             for col in mat2_t]
-            for row in mat1]
+    return [
+            [
+                sum(a * b for a, b in zip(row, col, strict=True))
+                for col in mat2_t
+            ] for row in mat1
+    ]
