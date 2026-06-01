487d2d9b99049505caec230b70d81568bdb3b75c
gcanavag <gcanavag@student.42luxembourg.lu>
2026-06-01 22:16:06 +0200

    n==0 returns None

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -11,6 +11,6 @@ def summation_i_squared(n):
     """
     if type(n) is float and n.is_integer():
         n = int(n)
-    if n is None or type(n) is bool or type(n) is not int or n < 0:
+    if n is None or type(n) is bool or type(n) is not int or n < 1:
         return None
     return (n * (n + 1) * (2 * n + 1)) // 6
