dd0f9318cb070cf551447befce22b52da4b770d3
Guillaume Canavaggio <guillaume.canavaggio@ai.dlh.lu>
2026-06-09 19:43:50 +0200

    Rem tests

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -16,15 +16,3 @@ class Poisson:
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
             self.lambtha = float(sum(data) / len(data))
-
-
-if __name__ == "__main__":
-    import numpy as np
-
-    np.random.seed(0)
-    data = np.random.poisson(5., 100).tolist()
-    p1 = Poisson(data)
-    print('Lambtha:', p1.lambtha)
-
-    p2 = Poisson(lambtha=5)
-    print('Lambtha:', p2.lambtha)
