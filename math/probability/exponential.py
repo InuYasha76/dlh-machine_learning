94067ee761f67d0fd4c8ad5252526c72a22b0cb3
Guillaume Canavaggio <guillaume.canavaggio@ai.dlh.lu>
2026-06-09 21:07:55 +0200

    Add CDF

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -32,3 +32,17 @@ class Exponential:
         e = 2.7182818285
 
         return self.lambtha * (e ** (-self.lambtha * x))
+
+    def cdf(self, x):
+        """
+        Calculates the value of the CDF for a given time period.
+        Args:
+            x (float): a time period [0; inf[
+        Returns:
+            float, the cdf value for x
+        """
+        if x < 0:
+            return 0
+        e = 2.7182818285
+
+        return 1 - (e ** (-self.lambtha * x))
