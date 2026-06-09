1101b383f0d534e1cd7c2430adf2b1b4b3e9cdfd
Guillaume Canavaggio <guillaume.canavaggio@ai.dlh.lu>
2026-06-09 20:01:05 +0200

    Add pmf compute

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -4,7 +4,8 @@
 
 class Poisson:
     """This class is about Poisson law."""
-    def __init__(self, data=None, lambtha=1.):
+
+    def __init__(self, data=None, lambtha=1.0):
         if data is None:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
@@ -16,3 +17,19 @@ class Poisson:
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
             self.lambtha = float(sum(data) / len(data))
+
+    def pmf(self, k):
+        """instance method, computes the PMF given k successes."""
+        if k < 0:
+            return 0.0
+        k = int(k)
+        e = 2.7182818285
+        pmf = e ** (-self.lambtha)
+
+        if k == 0:
+            return pmf
+
+        for j in range(1, k + 1):
+            pmf = pmf * self.lambtha / j
+
+        return pmf
