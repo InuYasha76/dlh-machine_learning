ccfea6c89d4ad7daed8638b3b29f9ae9390db549
Guillaume CANAVAGGIO <guillaume.canavaggio@ai.dlh.lu>
2026-07-06 22:12:22 +0200

    -import pandas

--- pipeline/pandas/4-array.py
+++ pipeline/pandas/4-array.py
@@ -2,9 +2,6 @@
 """This module is about pandas DataFrame."""
 
 
-import pandas as pd
-
-
 def array(df):
     """
     Converts the last 10 lines of columns High, Close of a dataframe
@@ -14,6 +11,6 @@ def array(df):
     Returns:
         A numpy.ndarray containing the last 10 lines of 'High' and 'Close'.
     """
-    if df is None or not isinstance(df, pd.DataFrame):
+    if df is None or type(df).__name__ !=  'DataFrame':
         return 0
     return df[['High', 'Close']].tail(10).to_numpy()
