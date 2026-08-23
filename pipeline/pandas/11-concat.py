a80a72df51f004a2e8c211b616b4d9bbc0f14eb3
InuYasha76 <guillaume.canavaggio@gmail.com>
2026-08-23 17:03:40 +0200

    Update 11-concat.py
    rem unnecessary blank and comment lines

--- pipeline/pandas/11-concat.py
+++ pipeline/pandas/11-concat.py
@@ -7,7 +7,8 @@ import pandas as pd
 
 def concat(df1, df2):
     """
-    Indexes two dataframes on their Timestamp columns and concatenates them.
+    Indexes two dataframes on their Timestamp columns and concatenates df1 
+    to df2.
     Args:
         df1 (pandas.DataFrame): coinbase dataframe.
         df2 (pandas.DataFrame): bitstamp dataframe.
@@ -15,14 +16,7 @@ def concat(df1, df2):
         A concatenated pandas DataFrame with multi-index keys.
     """
     index = __import__('10-index').index
-
-    """if df1 is None or type(df1).__name__ != 'DataFrame':
-        return 0
-    if df2 is None or type(df2).__name__ != 'DataFrame':
-        return 0
-    """
     df1 = index(df1)
     df2 = index(df2)
     df2 = df2.loc[:1417411920]
-
     return pd.concat([df2, df1], keys=['bitstamp', 'coinbase'])
