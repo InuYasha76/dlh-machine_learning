3d3a0ddfd0a904025393cfb2076edcaed8acea0d
InuYasha76 <guillaume.canavaggio@gmail.com>
2026-08-23 17:05:09 +0200

    Update 11-concat.py
    rem trailing space for pycodestyle compliance

--- pipeline/pandas/11-concat.py
+++ pipeline/pandas/11-concat.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 def concat(df1, df2):
     """
-    Indexes two dataframes on their Timestamp columns and concatenates df1 
+    Indexes two dataframes on their Timestamp columns and concatenates df1
     to df2.
     Args:
         df1 (pandas.DataFrame): coinbase dataframe.
@@ -15,8 +15,8 @@ def concat(df1, df2):
     Returns:
         A concatenated pandas DataFrame with multi-index keys.
     """
-    index = __import__('10-index').index
+    index = __import__("10-index").index
     df1 = index(df1)
     df2 = index(df2)
     df2 = df2.loc[:1417411920]
-    return pd.concat([df2, df1], keys=['bitstamp', 'coinbase'])
+    return pd.concat([df2, df1], keys=["bitstamp", "coinbase"])
