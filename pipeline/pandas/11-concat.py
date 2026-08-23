641b1c3000e066e9803e4715ffbc3958268fdc80
InuYasha76 <guillaume.canavaggio@gmail.com>
2026-08-23 17:11:49 +0200

    Update 11-concat.py
    move index import to top, improve comments and index naming.

--- pipeline/pandas/11-concat.py
+++ pipeline/pandas/11-concat.py
@@ -3,20 +3,21 @@
 
 
 import pandas as pd
+index_by_timestamp = __import__("10-index").index
 
 
 def concat(df1, df2):
     """
     Indexes two dataframes on their Timestamp columns and concatenates df1
-    to df2.
+    to df2 up to timestamp 1417411920
     Args:
         df1 (pandas.DataFrame): coinbase dataframe.
         df2 (pandas.DataFrame): bitstamp dataframe.
     Returns:
-        A concatenated pandas DataFrame with multi-index keys.
+        A concatenated pandas DataFrame with multi-index keys ("bitstamp",
+        "coinbase").
     """
-    index = __import__("10-index").index
-    df1 = index(df1)
-    df2 = index(df2)
+    df1 = index_by_timestamp(df1)
+    df2 = index_by_timestamp(df2)
     df2 = df2.loc[:1417411920]
     return pd.concat([df2, df1], keys=["bitstamp", "coinbase"])
