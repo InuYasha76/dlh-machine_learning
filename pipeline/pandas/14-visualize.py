2e3321d5de8a4a0b0aef03993c982745e928988b
InuYasha76 <guillaume.canavaggio@gmail.com>
2026-08-23 18:13:22 +0200

    Update 14-visualize.py
    Add comments and remove blank lines

--- pipeline/pandas/14-visualize.py
+++ pipeline/pandas/14-visualize.py
@@ -6,8 +6,27 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from_file = __import__('2-from_file').from_file
 
+"""
+This script performs the following:
+- Import the coinbase bitcoin dataset,
+- Remove the column 'Weighted_Price',
+- Rename the column 'Timestamp' to 'Date',
+- Convert the timestamp values to date values,
+- Index the data frame on 'Date',
+- Set missing values in 'Close' to the previous row value,
+- Set missing values in 'High', 'Low', 'Open' to the same row's 'Close' value,
+- Set missing values in 'Volume_(BTC)' and 'Volume_(Currency)' to 0,
+- Plot the data from 2017 and beyond at daily intervals and group the values
+  of the same day such that:
+  - 'High': max,
+  - 'Low': min,
+  - 'Open': mean,
+  - 'Close': mean,
+  - 'Volume_(BTC)': sum,
+  - 'Volume_(Currency)': sum,
+- Return the transformed pd.DataFrame before plotting.
+"""
 df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
-
 df = df.drop(columns=['Weighted_Price'], errors='ignored')
 df = df.rename(columns={'Timestamp': 'Date'})
 df['Date'] = pd.to_datetime(df['Date'], unit='s')
